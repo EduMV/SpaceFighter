c76388fc0eab62d21ebf85591358d54c65708ee5
EduMV <edu.marin.vera@gmail.com>
2022-07-03 17:47:45 -0500

    Movimiento de nave v.2

--- ship.py
+++ ship.py
@@ -1,20 +1,25 @@
 import tkinter as tk
+from PIL import ImageTk
+from PIL import Image
 import math
 
 class Ship:
-    angles = [3*math.pi/2, 5*math.pi/3, 11*math.pi/6] + [i* math.pi/6 for i in range(9)]
 
-    def __init__(self, canvas, sprites_dir, x, y):
+    def __init__(self, canvas, sprites_dir, x, y, ang):
         self.color = sprites_dir[0]
         self.canvas = canvas
-        self.img = tk.PhotoImage(file=sprites_dir + f"/{self.color}_ship0.png")
-        self.el = self.canvas.create_image(x, y, image=self.img)
+
+        self.ang = ang # ángulo inicial en sexagesimal
+        self.v = 3 # multiplicador de velocidad de la nave
+
+        self.img = Image.open(sprites_dir + f"/{self.color}_ship0.png") # Cargar imagen con pillow
+        self.tkimg = ImageTk.PhotoImage(self.img.rotate(self.get_img_ang())) # Rotar al angulo inicial
+
+        # Cargar tkimg al canvas y guardar el "elemento"
+        self.el = self.canvas.create_image(x, y, image=self.tkimg)
         
-        self.sprites_lst = [
-            tk.PhotoImage(file=sprites_dir + f"/{self.color}_ship{i}.png") for i in range(12)]
-        
-        self.ang = 3 * math.pi/2
-        self.v = 3
+    def get_img_ang(self):
+        return (360 - self.ang) - 90
 
     def get_position(self):
         return tuple(self.canvas.coords(self.el))
@@ -23,21 +28,18 @@ class Ship:
         self.canvas.move(self.el, x, y)
     
     def move_forward(self):
-        x = math.cos(self.ang) * self.v
-        y = math.sin(self.ang) * self.v
+        rads_ang = math.radians(self.ang)
+        x = math.cos(rads_ang) * self.v
+        y = math.sin(rads_ang) * self.v
         self.move(x, y)
     
     def rotate_ah(self, *args):
-        if self.ang > 0:
-            self.ang -= math.pi/64
-        else:
-            self.ang = 2 * math.pi - math.pi/64
+        self.ang -= 3
+        self.ang %= 360
     
     def rotate_h(self, *args):
-        if self.ang < 2 * math.pi:
-            self.ang += math.pi/64
-        else:
-            self.ang = 0 + math.pi/64
+        self.ang += 3
+        self.ang %= 360
     
     def delete(self):
         self.canvas.delete(self.el)
@@ -47,14 +49,9 @@ class Ship:
         self.update_sprite()
     
     def update_sprite(self):
-        min_val = 4
-        for i in range(len(self.angles)):
-            val = abs(self.ang-self.angles[i])
-            if val < min_val:
-                min_val = val
-                min_index = i
-
-        self.canvas.itemconfig(self.el,image=self.sprites_lst[min_index])
-        
+        x, y = self.get_position()
+        self.delete()
+        self.tkimg = ImageTk.PhotoImage(self.img.rotate(self.get_img_ang()))
+        self.el = self.canvas.create_image(x, y, image=self.tkimg)
 
             
--- space_fighter.py
+++ space_fighter.py
@@ -11,21 +11,36 @@ class SpaceFighter(tk.Tk):
 
         self.canvas = tk.Canvas(self, bg="darkblue", width=self.width, height=self.height)
         self.canvas.pack()
-        self.s1 = Ship(self.canvas, "w_ship_sprites", 100, 500)
-        #self.s2 = Ship(self.canvas, "b_ship_sprites", 700, 100)
-        self.events_init()
+        self.s1 = Ship(self.canvas, "w_ship_sprites", 100, 500, 270)
+        self.s2 = Ship(self.canvas, "b_ship_sprites", 700, 100, 90)
+        self.pressed = {}
+        self.bindings_init()
         self.game_loop()
 
-    def events_init(self):
-        self.bind("<Left>", self.s1.rotate_ah)
-        self.bind("<Right>", self.s1.rotate_h)
+    def bindings_init(self):
+        for key in ["Left", "Right", "a", "d"]:
+            self.bind(f"<KeyPress-{key}>", self.key_pressed)
+            self.bind(f"<KeyRelease-{key}>", self.key_released)
+            self.pressed[key] = False
+
+    def key_pressed(self, event):
+        self.pressed[event.keysym] = True
+
+    def key_released(self, event):
+        self.pressed[event.keysym] = False
+    
+    def check_presses(self):
+        if self.pressed["Left"]: self.s1.rotate_ah()
+        if self.pressed["Right"]: self.s1.rotate_h()
+        if self.pressed["a"]: self.s2.rotate_ah()
+        if self.pressed["d"]: self.s2.rotate_h()
+        self.canvas.update()
 
     def game_loop(self):
+        self.check_presses()
         self.s1.update()
-        print(self.s1.get_position())
+        self.s2.update()
         self.after(50, self.game_loop)
-
-    
 
 if __name__ == "__main__":
     jf = SpaceFighter()
